eb257c25e1023f8c2ce2ebc5114199f449c918c2
KanataGregori <chan26770@gmail.com>
2024-12-29 20:13:49 +0700

    Add files via upload

---
+++ Style.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-button').addEventListener('click', () => {
+    const navbar = document.getElementById('navbar');
+    navbar.style.display = navbar.style.display === 'block' ? 'none' : 'block';
+});
